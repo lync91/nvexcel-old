2aa3a59dbbf2b8b47889519d816246667d494e66
unknown <mr.lync91@gmail.com>
2020-03-23 22:16:17 +0700

    re test

--- src/taskpane/components/CharConvert.tsx
+++ src/taskpane/components/CharConvert.tsx
@@ -55,9 +55,6 @@ export class CharConvert extends React.Component<AppProps> {
 				console.log(`The range address was ${range.address}.`);
 				console.log(range.values);
 				const newValues = window['convertTo'](JSON.stringify(range.values), this.props.srcKey, this.props.descKey);
-				console.log(newValues);
-				console.log(newValues);
-				console.log(newValues);
 				
 				range.values = JSON.parse(newValues);
 			});
